3e2f60d52f866568349b9a29166dd3585678ff74
tzanu15 <tanuionut31@gmail.com>
2025-04-02 13:50:28 +0300

    Add frontend/src/main.jsx 1743591564393

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';
+
+// Create axios instance with default config
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:8000';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
